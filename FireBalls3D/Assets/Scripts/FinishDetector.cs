6df92e56905b9473d892b25e70bdb7b7c3b5f28d
Timofeev Mikhail <adventure686@mail.ru>
2021-04-27 21:19:20 +0300

    FinishDetector

---
+++ FireBalls3D/Assets/Scripts/FinishDetector.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class FinishDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnTowerBreak()
+    {
+        Debug.Log("You won!!!");
+    }
+}
--- FireBalls3D/Assets/Scripts/Tower.cs
+++ FireBalls3D/Assets/Scripts/Tower.cs
@@ -1,15 +1,18 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(TowerBuilder))]
 public class Tower : MonoBehaviour
 {
     [SerializeField] private TMP_Text _blockCount;
     private List<Block> _blocks = new List<Block>();
+    public event UnityAction Break;
     
     private void Start()
     {
+        Break += FindObjectOfType<FinishDetector>().OnTowerBreak;
         _blocks = GetComponent<TowerBuilder>().Build();
         _blockCount.text = _blocks.Count.ToString();
 
@@ -25,12 +28,13 @@ public class Tower : MonoBehaviour
         _blocks.Remove(block);
         _blockCount.text = _blocks.Count.ToString();
 
+        if (_blocks.Count == 0)
+            Break?.Invoke();
+
         var offset = new Vector3(0, block.transform.localScale.y, 0);
         foreach (var b in _blocks)
         {
             b.transform.position -= offset;
         }
-        
-        if(_blocks.Count == 0) Debug.Log("You won!");
     }
 }
